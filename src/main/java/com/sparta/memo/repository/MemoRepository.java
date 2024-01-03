69fb2bd3d8474b1df7f13b2058058243a64154d2
tls3254 <bikim9923@gmail.com>
2024-01-04 01:30:49 +0900

    Spring boot의 JPA

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -3,6 +3,8 @@ package com.sparta.memo.repository;
 import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -85,5 +87,13 @@ public class MemoRepository {
             }
         }, id);
     }
+    @Transactional
+    public Memo createMemo(EntityManager em) {
+        Memo memo = em.find(Memo.class, 1);
+        memo.setUsername("Robbie");
+        memo.setContents("@Transactional 전파 테스트 중!");
 
+        System.out.println("createMemo 메서드 종료");
+        return memo;
+    }
 }
